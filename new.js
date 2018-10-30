6fb8881892bde136e65a26da7769ef75dd83edc3
thispassing <thispassing@users.noreply.github.com>
2018-10-30 12:52:09 -0400

    learning js

---
+++ new.js
@@ -0,0 +1,3 @@
+var nice = prompt("Hi, have anything nice to say today?")
+var response = prompt(nice+", really?? Is that all you have to say?")
+console.log(nice+"\n"+response)
---
+++ part333.js
@@ -0,0 +1,4 @@
+var pounds = prompt("Enter a number in pounds:");
+var kilograms = pounds*0.454;
+alert("The weight is "+kilograms+" kilograms");
+console.log("Conversion Completed");
