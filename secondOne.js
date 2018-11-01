a738062a6496c005cab2c52312f7ccf708ac7e58
thispassing <thispassing@users.noreply.github.com>
2018-11-01 14:28:10 -0400

    for loops in js

---
+++ secondOne.js
@@ -0,0 +1,20 @@
+var num = 1
+
+while (true) {
+    if (num%2 === 0) {
+        console.log(num)
+    }
+    num = num+1;
+    if (num===11) {
+        break;
+    }
+}
+
+// var x = (1,2,3,4,5,6,7,8,9,10);
+// var n;
+
+
+// for (n in x){
+//     console.log(n)
+// }
+
