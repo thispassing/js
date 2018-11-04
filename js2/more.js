c6ff6bb237011f2e0797413fa2472b3a0d7d6b37
thispassing <thispassing@users.noreply.github.com>
2018-11-03 20:49:24 -0400

    m

---
+++ js2/more.js
@@ -0,0 +1,6 @@
+function doit(name) {
+    alert("haha "+name+" lolu")
+}
+
+var x = prompt("what yo name is?")
+doit(x)
---
+++ js2/we.js
@@ -0,0 +1,18 @@
+function luckySum(a, b, c){
+
+    if a==1{
+        var what = "omg"
+        alert (what)
+    }
+    if b==13{
+        alert (a)
+    }
+    if c==13{
+        alert (a+b)
+    }
+    alert (a+b+c)
+}
+
+luckySum(1, 2, 3)
+
+alert ("stfu")
